506a569e0eed2ba620619403add9e27b71463c0d
Mickey Cheong <mickey@cheo.ng>
2016-10-13 11:46:08 +0800

    Format the code according to PEP8 guidelines

--- media.py
+++ media.py
@@ -8,8 +8,8 @@ Movie object
 
 class Movie:
 
-    def __init__(self, title, storyline, poster_url, trailer_url):
+    def __init__(self, title):
         self.title = title
-        self.storyline = storyline
-        self.poster_image_url = poster_url
-        self.trailer_youtube_url = trailer_url
+        self.storyline = ""
+        self.poster_url = ""
+        self.trailer_url = ""
--- start.py
+++ start.py
@@ -28,19 +28,77 @@ if os.environ.get('TMDB_API', False):
         videos = m.videos()
         video = videos['results'][0]
         youtube_url = 'https://youtube.com/watch?v=' + video['key']
-        
+
         # Append Movie object
         movie = media.Movie(m.title, m.overview, poster_image_url, youtube_url)
         movies.append(movie)
 
 else:
-    avatar = media.Movie("Avatar", "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=-9ceBgWV8io")
-    deadpool = media.Movie("Deadpool", "A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.", "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg", "https://www.youtube.com/watch?v=gtTfd6tISfw")
-    ghostbusters = media.Movie("Ghostbusters", "Following a ghost invasion of Manhattan, paranormal enthusiasts Erin Gilbert and Abby Yates, nuclear engineer Jillian Holtzmann, and subway worker Patty Tolan band together to stop the otherworldly threat.", "https://upload.wikimedia.org/wikipedia/en/3/32/Ghostbusters_2016_film_poster.png", "https://www.youtube.com/watch?v=w3ugHP-yZXw")
-    olympus = media.Movie("Olympus Has Fallen", "Disgraced Secret Service agent (and former presidential guard) Mike Banning finds himself trapped inside the White House in the wake of a terrorist attack; using his inside knowledge, Banning works with national security to rescue the President from his kidnappers.", "https://upload.wikimedia.org/wikipedia/en/b/bf/Olympus_Has_Fallen_poster.jpg", "https://www.youtube.com/watch?v=vwx1f0kyNwI")
-    angry_birds = media.Movie("The Angry Birds Movie", "Find out why the birds are so angry. When an island populated by happy, flightless birds is visited by mysterious green piggies, it's up to three unlikely outcasts - Red, Chuck and Bomb - to figure out what the pigs are up to.", "https://upload.wikimedia.org/wikipedia/en/f/f9/The_Angry_Birds_Movie_poster.png", "https://www.youtube.com/watch?v=1U2DKKqxHgE")
-    iron_man = media.Movie("Iron Man", "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.", "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG", "https://www.youtube.com/watch?v=8hYlB38asDY")
 
-    movies = [avatar, deadpool, ghostbusters, olympus, angry_birds, iron_man]
+    # Avatar
+    avatar = media.Movie("Avatar")
+    avatar.storyline = ("A paraplegic marine dispatched to the moon Pandora "
+                        "on a unique mission becomes torn between following "
+                        "his orders and protecting the world he feels is "
+                        "his home.")
+    avatar.poster_url = ("https://upload.wikimedia.org/wikipedia/"
+                         "en/b/b0/Avatar-Teaser-Poster.jpg")
+    avatar.trailer_url = "https://www.youtube.com/watch?v=-9ceBgWV8io"
+
+    # Deadpool
+    deadpool = media.Movie("Deadpool")
+    deadpool.storyline = ("A fast-talking mercenary with a morbid sense of "
+                          "humor is subjected to a rogue experiment that "
+                          "leaves him with accelerated healing powers and a "
+                          "quest for revenge.")
+    deadpool.poster_url = ("https://upload.wikimedia.org/wikipedia/en/4/46/"
+                           "Deadpool_poster.jpg")
+    deadpool.trailer_url = "https://www.youtube.com/watch?v=gtTfd6tISfw"
+
+    # Ghostbusters
+    ghostbusters = media.Movie("Ghostbusters")
+    ghostbusters.storyline = ("Following a ghost invasion of Manhattan, "
+                              "paranormal enthusiasts Erin Gilbert and Abby "
+                              "Yates, nuclear engineer Jillian Holtzmann, "
+                              "and subway worker Patty Tolan band together "
+                              "to stop the otherworldly threat.")
+    ghostbusters.poster_url = ("https://upload.wikimedia.org/wikipedia/"
+                               "en/3/32/Ghostbusters_2016_film_poster.png")
+    ghostbusters.trailer_url = "https://www.youtube.com/watch?v=w3ugHP-yZXw"
+
+    # Olympus
+    olympus = media.Movie("Olympus Has Fallen")
+    olympus.storyline = ("Disgraced Secret Service agent (and former "
+                         "presidential guard) Mike Banning finds himself "
+                         "trapped inside the White House in the wake of a "
+                         "terrorist attack; using his inside knowledge, "
+                         "Banning works with national security to rescue "
+                         "the President from his kidnappers.")
+    olympus.poster_url = ("https://upload.wikimedia.org/wikipedia/en/b/bf/"
+                          "Olympus_Has_Fallen_poster.jpg")
+    olympus.trailer_url = "https://www.youtube.com/watch?v=vwx1f0kyNwI"
+
+    # Angry Birds
+    angry_birds = media.Movie("The Angry Birds Movie")
+    angry_birds.storyline = ("Find out why the birds are so angry. When an "
+                             "island populated by happy, flightless birds "
+                             "is visited by mysterious green piggies, it's "
+                             "up to three unlikely outcasts - Red, Chuck "
+                             "and Bomb - to figure out what the pigs are up "
+                             "to.")
+    angry_birds.poster_url = ("https://upload.wikimedia.org/wikipedia/en/f/"
+                              "f9/The_Angry_Birds_Movie_poster.png")
+    angry_birds.trailer_url = "https://www.youtube.com/watch?v=1U2DKKqxHgE"
+
+    # Ironman
+    ironman = media.Movie("Iron Man")
+    ironman.storyline = ("After being held captive in an Afghan cave, "
+                         "billionaire engineer Tony Stark creates a unique "
+                         "weaponized suit of armor to fight evil.")
+    ironman.poster_url = ("https://upload.wikimedia.org/wikipedia/en/7/70/"
+                          "Ironmanposter.JPG")
+    ironman.trailer_url = "https://www.youtube.com/watch?v=8hYlB38asDY"
+
+    movies = [avatar, deadpool, ghostbusters, olympus, angry_birds, ironman]
 
 ft.open_movies_page(movies)
